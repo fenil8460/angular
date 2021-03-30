82fdae6c46024e2253789b7a727f80399757c58d
Abhijitsinh <discussion4all@yahoo.com>
2021-03-30 17:18:20 +0530

    11

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  getText:String = '';
+  name2 = 'fenil';
+  disableBox=true
+  getName(name:String){
+    console.log(name);
+    this.getText = name
+  };
+  enableBox(){
+    this.disableBox = !this.disableBox;
+  }
 }
+// events
+// click events
+// keyup event
+// keyup with enter and space
+// keydown
+// blur
+// mouseover and mouseleave
+// get value on textbox 
